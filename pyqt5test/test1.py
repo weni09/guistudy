df2d14dbcf84cd5fde3155d431ed352fb60166cb
weni09 <weni09@163.com>
2020-07-01 00:39:12 +0800

    2020-07-01 0:38学习记录

--- pyqt5test/test1.py
+++ pyqt5test/test1.py
@@ -8,17 +8,40 @@
 import sys
 from PyQt5.Qt import *
 
-''''
-def getSubClasses(cls):
-    for subcls in cls.__subclasses__():
-        print(subcls)
-        if len(cls.__subclasses__())>0:
-            getSubClasses(subcls)
-getSubClasses(QWidget)
-
-'''
 class window(QWidget):
     def __init__(self):
         super().__init__()
         self.setup_ui()
     def setup_ui(self):
+
+
+        obj0 = QObject()
+        obj1 = QObject()
+        obj2 = QObject()
+        obj3 = QObject()
+        obj4 = QObject()
+        obj5 = QObject()
+        obj2.setObjectName("2")
+        obj3.setObjectName("3")
+        print("obj0",obj0)
+        print("obj1",obj1)
+        print("obj2",obj2)
+        print("obj3",obj3)
+        print("obj4",obj4)
+        print("obj5",obj5)
+
+        obj1.setParent(obj0)
+        obj2.setParent(obj0)
+        obj3.setParent(obj1)
+        obj4.setParent(obj2)
+        obj5.setParent(obj2)
+
+        # print(obj0.children())
+        # print(obj0.findChildren(QObject,None,Qt.FindDirectChildrenOnly))
+        # print(obj0.findChild(QObject,"3",Qt.FindChildrenRecursively))
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    wd = window()
+    #wd.show()
+    sys.exit(app.exec())
--- pyqt5test/test2.py
+++ pyqt5test/test2.py
@@ -6,50 +6,10 @@
 # @File : test2.py
 #===================================================
 
-
-import sys
-from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
-    QTextEdit, QGridLayout, QApplication)
-
-class Example(QWidget):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-
-    def initUI(self):
-
-        title = QLabel('Title')
-        author = QLabel('Author')
-        review = QLabel('Review')
-
-        titleEdit = QLineEdit()
-        authorEdit = QLineEdit()
-        reviewEdit = QTextEdit()
-
-        grid = QGridLayout()
-        grid.setSpacing(10)
-
-        grid.addWidget(title, 1, 0)
-        grid.addWidget(titleEdit, 1, 1)
-
-        grid.addWidget(author, 2, 0)
-        grid.addWidget(authorEdit, 2, 1)
-
-        grid.addWidget(review, 3, 0)
-        grid.addWidget(reviewEdit, 3, 1,5,1)
-
-        self.setLayout(grid)
-
-        self.setGeometry(300, 300, 350, 300)
-        self.setWindowTitle('Review')
-        self.show()
-
-####测试git修改提交
-if __name__ == '__main__':
-
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+from PyQt5.Qt import *
+def getSubClasses(cls):
+    for subcls in cls.__subclasses__():
+        print(subcls)
+        if len(cls.__subclasses__())>0:
+            getSubClasses(subcls)
+getSubClasses(QWidget)
